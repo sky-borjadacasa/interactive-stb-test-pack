5f8f233271c99578f1131b84bb7ddd799ea6798d
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-05-29 15:08:54 +0100

    Try to make first test work properly

--- tests/mySky_test.py
+++ tests/mySky_test.py
@@ -4,6 +4,11 @@ import stbt
 
 def test_open_mysky():
 	stbt.press('KEY_YELLOW')
-	assert stbt.wait_until(lambda: find_selection().text == "loading...")
 	assert stbt.wait_for_match('mySky/SkyTopLogo.png')
+	assert stbt.wait_until(lambda: find_selection().text == "Good afternoon")
 	print stbt.ocr()
+	clear_test()
+
+def clear_test():
+	while stbt.wait_for_match('mySky/SkyTopLogo.png'):
+		stbt.press('KEY_BACKUP')
