2e488992b0bb1214aa26ad70a234dd4e4d227a28
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-05-29 15:04:22 +0100

    Improve test code

--- tests/mySky_test.py
+++ tests/mySky_test.py
@@ -4,22 +4,6 @@ import stbt
 
 def test_open_mysky():
 	stbt.press('KEY_YELLOW')
+	assert stbt.wait_until(lambda: find_selection().text == "loading...")
 	assert stbt.wait_for_match('mySky/SkyTopLogo.png')
-
-### Old examples:
-def test_that_live_tv_is_playing():
-    stbt.press('KEY_CLOSE')  # Close any open menus
-    assert stbt.wait_for_motion()
-
-
-def test_that_stb_tester_logo_is_shown():
-    stbt.press('KEY_CHANNELUP')
-    assert stbt.wait_for_match('stb-tester-logo.png')
-
-
-def test_read_menu():
-    stbt.press('KEY_CLOSE')
-    sleep(1)
-    stbt.press('KEY_MENU')
-    sleep(1)
-    print stbt.ocr()
+	print stbt.ocr()
