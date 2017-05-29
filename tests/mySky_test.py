55c82e12373bc7316ad5c272987a2dcf4ff09ccd
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-05-29 14:37:51 +0100

    Added initial test file for MySky

---
+++ tests/mySky_test.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+import stbt
+
+def test_open_mysky():
+	stbt.press('KEY_YELLOW')
+	assert stbt.wait_for_match('images/blob_sky_logo.jpg')
+
+### Old examples:
+def test_that_live_tv_is_playing():
+    stbt.press('KEY_CLOSE')  # Close any open menus
+    assert stbt.wait_for_motion()
+
+
+def test_that_stb_tester_logo_is_shown():
+    stbt.press('KEY_CHANNELUP')
+    assert stbt.wait_for_match('stb-tester-logo.png')
+
+
+def test_read_menu():
+    stbt.press('KEY_CLOSE')
+    sleep(1)
+    stbt.press('KEY_MENU')
+    sleep(1)
+    print stbt.ocr()
