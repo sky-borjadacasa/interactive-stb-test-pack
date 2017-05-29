60d913710b00e37afa0c1108f55a6e423cbbf128
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-05-29 15:19:35 +0100

    Try to match text during the text

--- tests/mySky_test.py
+++ tests/mySky_test.py
@@ -4,8 +4,8 @@ import stbt
 
 def test_open_mysky():
 	stbt.press('KEY_YELLOW')
+	assert stbt.wait_until(stbt.match_text('loading...'))
 	assert stbt.wait_for_match('mySky/SkyTopLogo.png')
-	assert stbt.wait_until(lambda: find_selection().text == "Good afternoon")
 	print stbt.ocr()
 	clear_test()
 
