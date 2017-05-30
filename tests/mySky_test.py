bcec195cc30a6ee572cb7552c396c53a5284995a
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-05-30 16:16:57 +0100

    Improved test code

--- tests/mySky_test.py
+++ tests/mySky_test.py
@@ -1,19 +1,29 @@
+#!/usr/bin/env python
+"""
+Test cases for MySky
+"""
+
 from time import sleep
 
 import stbt
 
 def test_open_mysky():
-	try:
-		stbt.press('KEY_YELLOW')
-		assert stbt.wait_for_match('images/Loading.png')
-		assert stbt.wait_for_match('images/SkyTopLogo.png')
-		sleep(10)
-		m = stbt.match_text("Good afternoon")
-		assert m.match
-		print stbt.ocr()
-	finally:
-		clear_test()
+    """Open MySky app"""
+    try:
+        stbt.press('KEY_YELLOW')
+        # Find the loading text:
+        loading_region = stbt.Region(930, 0, width=350, height=150)
+        assert stbt.wait_for_match('images/Loading.png', region=loading_region)
+        assert stbt.wait_for_match('images/SkyTopLogo.png')
+        sleep(10)
+        match = stbt.match_text("Good afternoon")
+        assert match.match
+        print stbt.ocr()
+    finally:
+        sleep(1)
+        clear_test()
 
 def clear_test():
-	while stbt.wait_for_match('images/SkyTopLogo.png'):
-		stbt.press('KEY_BACKUP')
+    """Close MySky app"""
+    while stbt.wait_for_match('images/SkyTopLogo.png'):
+        stbt.press('KEY_BACKUP')
