0b5bf5c2ddaf728d3639240f32913889edd451d4
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-05-31 10:51:15 +0100

    Don’t try to find the loading text

--- tests/mySky_test.py
+++ tests/mySky_test.py
@@ -11,14 +11,10 @@ def test_open_mysky():
     """Open MySky app"""
     try:
         stbt.press('KEY_YELLOW')
-        # Find the loading text:
-        loading_region = stbt.Region(930, 0, width=350, height=150)
-        assert stbt.wait_for_match('images/Loading.png', region=loading_region)
         assert stbt.wait_for_match('images/SkyTopLogo.png')
         sleep(10)
         match = stbt.match_text("Good afternoon")
         assert match.match
-        print stbt.ocr()
     finally:
         clear_test()
 
