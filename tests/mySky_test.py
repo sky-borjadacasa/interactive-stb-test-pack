9c79e51e98cbe73b0162a29960263f4fc784e49b
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-05-31 14:13:29 +0100

    More testing

--- tests/mySky_test.py
+++ tests/mySky_test.py
@@ -19,10 +19,11 @@ def test_open_mysky2():
     """Open MySky app"""
     try:
         stbt.press('KEY_YELLOW')
+        assert stbt.wait_for_match('images/SkyTopLogo.png')
         sleep(10)
         region = stbt.Region(880, 0, width=400, height=720)
         # Just for testing:
-        ocr_result = stbt.ocr()
+        ocr_result = stbt.ocr(region=region)
         print ocr_result
 
         match = stbt.match_text("Good afternoon", region=region)
