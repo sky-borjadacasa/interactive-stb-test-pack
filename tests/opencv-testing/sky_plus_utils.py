b480a35b3a4726d17f3b50f1b70cb33f14c3db46
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-06-02 14:17:36 +0100

    Code cleaning

--- tests/opencv-testing/sky_plus_utils.py
+++ tests/opencv-testing/sky_plus_utils.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 # Constants:
 TM_CCOEFF_THRESHOLD = 100000000 # Maybe it should be 50000000
 TM_CCOEFF_NORMED_THRESHOLD = 0.8
+MAX_MATCHING_LOOPS = 15
 
 # Image files:
 IMAGE_BORDER = 'images/Border.png'
@@ -42,54 +43,46 @@ class MySkyMenuItem:
 
 # XXX - Testing code
 
-img = cv2.imread(TEST_IMAGE_MYSKY_MENU, 0)
-img2 = img.copy()
+original_image = cv2.imread(TEST_IMAGE_MYSKY_HOME, 0)
 template = cv2.imread(IMAGE_BORDER, 0)
 mask = cv2.imread(IMAGE_MASK, 0)
 w, h = template.shape[::-1]
 
-# All the 6 methods for comparison in a list
-methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', \
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-methods = ['cv2.TM_CCOEFF']
+# These are the 6 methods for comparison in a list:
+# cv2.TM_CCOEFF
+# cv2.TM_CCOEFF_NORMED
+# cv2.TM_CCORR
+# cv2.TM_CCORR_NORMED
+# cv2.TM_SQDIFF
+# cv2.TM_SQDIFF_NORMED
 
+
+method = cv2.TM_CCOEFF
+img = original_image.copy()
 menu_items = []
 
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
+# Apply template Matching
+count = 0
+while count < MAX_MATCHING_LOOPS:
+    count += 1
+    res = cv2.matchTemplate(img, template, method, mask)
 
-    # Apply template Matching
-    count = 0
-    threshold = 100000000
-    threshold = 50000000
-    while count < 15:
-        count += 1
-        res = cv2.matchTemplate(img, template, method, mask)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    print '{0}, {1}'.format(min_val, max_val)
+    if max_val < TM_CCOEFF_THRESHOLD:
+        continue
 
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-        print '{0}, {1}'.format(min_val, max_val)
-        if max_val < threshold:
-            continue
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    item = MySkyMenuItem(top_left, bottom_right)
+    menu_items.append(item)
 
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-            top_left = min_loc
-            bottom_right = max_loc
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-            item = MySkyMenuItem(top_left, bottom_right)
-            menu_items.append(item)
-        else:
-            top_left = max_loc
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-            item = MySkyMenuItem(top_left, bottom_right)
-            menu_items.append(item)
-
-        # Hide match under black rectangle:
-        cv2.rectangle(img, item.top_left, item.bottom_right, (0, 0, 0), -1)
+    # Hide match under black rectangle:
+    cv2.rectangle(img, item.top_left, item.bottom_right, (0, 0, 0), -1)
 
 # Show all menus found:
-print_image = img2.copy()
+print_image = original_image.copy()
 menu_items.sort(key=lambda x: x.top_left[1])
 
 for item in menu_items:
@@ -100,6 +93,6 @@ plt.subplot(121), plt.imshow(res, cmap='gray')
 plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
 plt.subplot(122), plt.imshow(print_image, cmap='gray')
 plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-plt.suptitle(meth)
+plt.suptitle('Method: TM_CCOEFF')
 
 plt.show()
