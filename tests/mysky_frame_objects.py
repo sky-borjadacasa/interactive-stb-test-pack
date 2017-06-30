8008e04bea5f2741fbb0e05a5ad1b2e91662179c
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-06-30 15:31:44 +0100

    Fixed typo

--- tests/mysky_frame_objects.py
+++ tests/mysky_frame_objects.py
@@ -26,7 +26,7 @@ class MySkyMainMenu(FrameObject):
 	@property
 	def message(self):
 		utils = SkyPlusTestUtils(self._frame)
-		text, _ utils.find_text_in_box(sky_plus_utils.MY_SKY_GREETING_REGION)
+		text, _ = utils.find_text_in_box(sky_plus_utils.MY_SKY_GREETING_REGION)
 		return text
 
 	@property
