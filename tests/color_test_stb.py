44893f7054794f0179dfa9987e4fcbb9ff963170
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-07-21 10:39:47 +0100

    Improve setup

--- tests/color_test_stb.py
+++ tests/color_test_stb.py
@@ -28,10 +28,7 @@ import stbt
 from stbt import FrameObject, match, MatchParameters, ocr, Region
 from time import sleep
 
-# os.system('sudo apt-get -y install python-scipy')
-install_and_import('scipy.stats', 'scipy')
 from scipy.stats import itemfreq
-install_and_import('numpy')
 import numpy as np
 install_and_import('cv2', 'opencv-python')
 
