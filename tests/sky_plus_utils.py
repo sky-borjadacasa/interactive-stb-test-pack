9f58a6091d99a9452af5113889bcff87e0a737f6
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-07-24 15:02:15 +0100

    Fixed errors and remove more unneeded code

--- tests/sky_plus_utils.py
+++ tests/sky_plus_utils.py
@@ -26,6 +26,10 @@ except ImportError:
     import tesserocr
 
 # Constants:
+# Regions:
+MY_SKY_REGION = ((880, 0), (1280 - 1, 720 - 1)) # The 400 pixels to the right and the whole height of the screen
+MY_SKY_GREETING_REGION = ((930, 90), (1230, 135))
+MY_SKY_TEXT_MENU_REGION = ((920, 125), (1240, 550))
 
 # Text recognition:
 OCR_CHAR_WHITELIST = string.ascii_letters + ' ' + string.digits
