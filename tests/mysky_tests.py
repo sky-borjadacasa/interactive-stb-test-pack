62472e41a5c7f25e081b0e4e5aabb2d307cd6200
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-12-14 14:02:47 +0000

    Small refactor

--- tests/mysky_tests.py
+++ tests/mysky_tests.py
@@ -19,23 +19,10 @@ def test_smoke_open_mysky():
     finally:
         mysky_test_utils.clear_test()
 
-#def test_acceptance_yellow_button_exits():
-def test_acceptance_yellow_button_exits():
-    """Open MySky app"""
-    mysky_test_utils.clear_test()
-    try:
-        sky_plus_utils.go_to_channel(mysky_constants.CHANNEL_SKY_ONE)
-        mysky_test_utils.open_and_check_mysky()
-
-        # Press yellow button:
-        stbt.press('KEY_YELLOW')
-        assert stbt.wait_until(lambda: not MySkyMainMenu().is_visible)
-
-    finally:
-        mysky_test_utils.clear_test()
-
 def test_acceptance_open_mysky():
-    """Open MySky app"""
+    """Open MySky app and navigate
+    Automates: https://interactiveqa.testrail.net/index.php?/cases/view/20
+    """
     mysky_test_utils.clear_test()
     try:
         sky_plus_utils.go_to_channel(mysky_constants.CHANNEL_SKY_ONE)
@@ -50,6 +37,21 @@ def test_acceptance_open_mysky():
         assert stbt.wait_until(lambda: MySkyMainMenu().message == mysky_constants.STRING_MANAGE_YOUR_ACCOUNT)
         stbt.press('KEY_UP')
         assert stbt.wait_until(lambda: MySkyMainMenu().message == mysky_constants.STRING_FIND_OUT_MORE)
+    finally:
+        mysky_test_utils.clear_test()
+
+#def test_acceptance_yellow_button_exits():
+def test_acceptance_yellow_button_exits():
+    """Open MySky app"""
+    mysky_test_utils.clear_test()
+    try:
+        sky_plus_utils.go_to_channel(mysky_constants.CHANNEL_SKY_ONE)
+        mysky_test_utils.open_and_check_mysky()
+
+        # Press yellow button:
+        stbt.press('KEY_YELLOW')
+        assert stbt.wait_until(lambda: not MySkyMainMenu().is_visible)
+
     finally:
         mysky_test_utils.clear_test()
 
