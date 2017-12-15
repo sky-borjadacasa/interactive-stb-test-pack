aa9ffae9dfb4d7e634ab11bbda09bfebf20ac2c0
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-12-15 14:55:44 +0000

    Disable bad check for now

--- tests/mysky_test_utils.py
+++ tests/mysky_test_utils.py
@@ -62,10 +62,11 @@ def open_and_check_mysky():
     menu = open_and_basic_check_mysky()
     menu_items = menu.menu_items
 
-    item = [x for x in menu_items if x.text == mysky_constants.STRING_FIND_OUT_MORE][0]
-    match_result = match(mysky_constants.MENU_FIND_OUT_MORE, frame=menu._frame, region=item.region)
-    debug('match_result: {0}{1}'.format(match_result.match, match_result.first_pass_result))
-    assert match_result.match, '[MySky] Could not find {0} menu'.format(mysky_constants.STRING_FIND_OUT_MORE)
+    # We can not know for sure which image to expect here
+    # item = [x for x in menu_items if x.text == mysky_constants.STRING_FIND_OUT_MORE][0]
+    # match_result = match(mysky_constants.MENU_FIND_OUT_MORE, frame=menu._frame, region=item.region)
+    # debug('match_result: {0}{1}'.format(match_result.match, match_result.first_pass_result))
+    # assert match_result.match, '[MySky] Could not find {0} menu'.format(mysky_constants.STRING_FIND_OUT_MORE)
     item = [x for x in menu_items if x.text == mysky_constants.STRING_MANAGE_YOUR_ACCOUNT][0]
     match_result = match(mysky_constants.MENU_MANAGE_YOUR_ACCOUNT, frame=menu._frame, region=item.region)
     debug('match_result: {0}{1}'.format(match_result.match, match_result.first_pass_result))
