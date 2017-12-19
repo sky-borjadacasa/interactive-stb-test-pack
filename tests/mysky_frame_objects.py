ca21004fe18adb14af468cdda7fb56b3ef07169f
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2017-12-19 13:57:12 +0000

    Testing

--- tests/mysky_frame_objects.py
+++ tests/mysky_frame_objects.py
@@ -34,7 +34,8 @@ def detect_moving_balls(frame):
     for i in range(1, 4):
         file = mysky_constants.MOVING_BALLS.format(i)
         debug('[MOVING BALLS] Using file: {0}'.format(file))
-        moving_balls = stbt.match(file, region=mysky_constants.MY_SKY_MOVING_BALLS_REGION, frame=frame)
+        #moving_balls = stbt.match(file, frame=frame, region=mysky_constants.MY_SKY_MOVING_BALLS_REGION)
+        moving_balls = stbt.match(file, frame=frame)
         if moving_balls:
             return True
     return False
