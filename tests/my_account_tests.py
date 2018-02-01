d4c0b37d6f80f050561a6068f3e97bd89ef1dbdb
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2018-02-01 16:03:54 +0000

    Testing code

--- tests/my_account_tests.py
+++ tests/my_account_tests.py
@@ -6,9 +6,10 @@ Test cases for My Account
 
 import stbt
 import sky_plus_utils
-from sky_plus_utils import clear_test
+from sky_plus_utils import clear_test, debug
 from interactive_frame_objects import InteractiveMainMenu, MyAccountMenu
 import interactive_constants
+from interactive_constants import MAIN_MENU_ITEM_REGIONS
 import sky_plus_strings
 from interactive_test_utils import open_and_basic_check_interactive_menu
 
@@ -21,7 +22,10 @@ def test_smoke_open_my_account():
 
         # Navigate menus:
         # pylint: disable=unused-variable
-        for i in range(0, 7):
+        for i in range(0, len(MAIN_MENU_ITEM_REGIONS)):
+            imenu = stbt.wait_until(InteractiveMainMenu, timeout_secs=20)
+            debug('[INTERACTIVE_MENU] Item selected: {0}'.format(imenu.message))
+
             stbt.press('KEY_DOWN')
         assert stbt.wait_until(lambda: InteractiveMainMenu().message == sky_plus_strings.MY_ACCOUNT, timeout_secs=20), \
             '[Interactive] Selected item is not [{0}]'.format(sky_plus_strings.MY_ACCOUNT)
