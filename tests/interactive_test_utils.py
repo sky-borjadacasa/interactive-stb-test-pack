66b220725ce69d262797fe842ee0ddd027bed985
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2018-02-02 11:21:55 +0000

    Code refactor and improvement

--- tests/interactive_test_utils.py
+++ tests/interactive_test_utils.py
@@ -7,6 +7,7 @@ Test cases for My Messages
 from time import sleep
 import stbt
 from sky_plus_utils import debug
+from interactive_constants import MAIN_MENU_ITEM_REGIONS
 from interactive_frame_objects import InteractiveMainMenu
 
 def open_and_basic_check_interactive_menu():
@@ -23,3 +24,30 @@ def open_and_basic_check_interactive_menu():
     debug(len(menu_items))
     assert len(menu_items) == 9, '[Interactive] Main menu should have 9 items, but has {0}'.format(len(menu_items))
     return menu
+
+def enter_menu(menu_name):
+    """Select menu with the given name
+
+    Args:
+        menu_name (str): Name of the menu to open
+    """
+    # Navigate menus:
+    menu = None
+    # pylint: disable=unused-variable
+    for i in range(0, len(MAIN_MENU_ITEM_REGIONS)):
+        menu = stbt.wait_until(InteractiveMainMenu, timeout_secs=20)
+        debug('[INTERACTIVE_MENU] Item selected: {0}'.format(menu.message))
+
+        if menu.message == menu_name:
+            debug('[INTERACTIVE_MENU] Item found!: {0}'.format(menu.message))
+            break
+        debug('[INTERACTIVE_MENU] Press DOWN')
+        stbt.press('KEY_DOWN')
+        # Give STB 3 seconds to move the highlighted menu entry
+        sleep(3)
+
+    assert menu.message == menu_name, \
+        '[INTERACTIVE_MENU] Selected item is not [{0}]'.format(menu_name)
+
+    # Open My Messages:
+    stbt.press('KEY_SELECT')
--- tests/my_account_tests.py
+++ tests/my_account_tests.py
@@ -4,46 +4,23 @@
 Test cases for My Account
 """
 
-from time import sleep
 import stbt
 import sky_plus_utils
-from sky_plus_utils import clear_test, debug
-from interactive_frame_objects import InteractiveMainMenu, MyAccountMenu
+from sky_plus_utils import clear_test
+from interactive_frame_objects import MyAccountMenu
 import interactive_constants
-from interactive_constants import MAIN_MENU_ITEM_REGIONS
 import sky_plus_strings
-from interactive_test_utils import open_and_basic_check_interactive_menu
+from interactive_test_utils import open_and_basic_check_interactive_menu, enter_menu
 
 def test_smoke_open_my_account():
     """Open MySky app"""
     clear_test()
     try:
         sky_plus_utils.go_to_channel(interactive_constants.CHANNEL_SKY_ONE)
-        # XXX Get menu here?
-        menu = open_and_basic_check_interactive_menu()
+        open_and_basic_check_interactive_menu()
+        enter_menu(sky_plus_strings.MY_ACCOUNT)
 
-        # Navigate menus:
-        # pylint: disable=unused-variable
-        for i in range(0, len(MAIN_MENU_ITEM_REGIONS)):
-            menu = stbt.wait_until(InteractiveMainMenu, timeout_secs=20)
-            debug('[INTERACTIVE_MENU] Item selected: {0}'.format(menu.message))
-
-            # Wait 3 secs and check
-            if menu.message == sky_plus_strings.MY_ACCOUNT:
-                debug('[INTERACTIVE_MENU] Item found!: {0}'.format(menu.message))
-                break
-            debug('[INTERACTIVE_MENU] Press DOWN')
-            stbt.press('KEY_DOWN')
-            sleep(3)
-        # XXX Is this needed?
-        # assert stbt.wait_until(lambda: InteractiveMainMenu().message == sky_plus_strings.MY_ACCOUNT, timeout_secs=20), \
-        #     '[Interactive] Selected item is not [{0}]'.format(sky_plus_strings.MY_ACCOUNT)
-        assert menu.message == sky_plus_strings.MY_ACCOUNT, \
-            '[INTERACTIVE_MENU] Selected item is not [{0}]'.format(sky_plus_strings.MY_ACCOUNT)
-
-        # Open My Messages:
-        stbt.press('KEY_SELECT')
-        ma_menu = stbt.wait_until(MyAccountMenu, timeout_secs=20)
-        assert ma_menu.is_visible, '[My Account] Menu is not visible'
+        menu = stbt.wait_until(MyAccountMenu, timeout_secs=20)
+        assert menu.is_visible, '[My Account] Menu is not visible'
     finally:
         clear_test()
