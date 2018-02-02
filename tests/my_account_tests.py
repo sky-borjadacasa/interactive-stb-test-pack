8e6ac8e16318b1d4704e051921d478fa21d4c3ab
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2018-02-02 10:50:42 +0000

    Testing more

--- tests/my_account_tests.py
+++ tests/my_account_tests.py
@@ -20,23 +20,25 @@ def test_smoke_open_my_account():
     try:
         sky_plus_utils.go_to_channel(interactive_constants.CHANNEL_SKY_ONE)
         # XXX Get menu here?
-        open_and_basic_check_interactive_menu()
+        menu = open_and_basic_check_interactive_menu()
 
         # Navigate menus:
         # pylint: disable=unused-variable
         for i in range(0, len(MAIN_MENU_ITEM_REGIONS)):
-            imenu = stbt.wait_until(InteractiveMainMenu, timeout_secs=20)
-            debug('[INTERACTIVE_MENU] Item selected: {0}'.format(imenu.message))
+            menu = stbt.wait_until(InteractiveMainMenu, timeout_secs=20)
+            debug('[INTERACTIVE_MENU] Item selected: {0}'.format(menu.message))
 
             # Wait 3 secs and check
             sleep(3)
-            if imenu.message == sky_plus_strings.MY_ACCOUNT:
-                debug('[INTERACTIVE_MENU] Item found!: {0}'.format(imenu.message))
+            if menu.message == sky_plus_strings.MY_ACCOUNT:
+                debug('[INTERACTIVE_MENU] Item found!: {0}'.format(menu.message))
                 break
             debug('[INTERACTIVE_MENU] Press DOWN')
             stbt.press('KEY_DOWN')
         # XXX Is this needed?
-        assert stbt.wait_until(lambda: InteractiveMainMenu().message == sky_plus_strings.MY_ACCOUNT, timeout_secs=20), \
+        # assert stbt.wait_until(lambda: InteractiveMainMenu().message == sky_plus_strings.MY_ACCOUNT, timeout_secs=20), \
+        #     '[Interactive] Selected item is not [{0}]'.format(sky_plus_strings.MY_ACCOUNT)
+        assert menu.message == sky_plus_strings.MY_ACCOUNT,
             '[Interactive] Selected item is not [{0}]'.format(sky_plus_strings.MY_ACCOUNT)
 
         # Open My Messages:
