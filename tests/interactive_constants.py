4431496e48cac8ed2876f3a4263880f0a03a0fd9
Pereira, Borja (Tester) <borja.pereira@sky.uk>
2018-06-05 10:07:19 +0100

    More refactoring

--- tests/interactive_constants.py
+++ tests/interactive_constants.py
@@ -15,16 +15,6 @@ TITLE_REGION = Region(45, 30, width=170, height=45)
 HELP_TITLE_REGION = Region(45, 85, width=210, height=35)
 GET_HELP_REGION = Region(45, 120, width=465, height=30)
 
-MAIN_MENU_ITEM_REGIONS = [Region(100, 338, width=530, height=32),
-                          Region(100, 374, width=530, height=32),
-                          Region(100, 410, width=530, height=32),
-                          Region(100, 446, width=530, height=32),
-                          Region(100, 482, width=530, height=32),
-                          Region(100, 518, width=530, height=32),
-                          Region(100, 554, width=530, height=32),
-                          Region(100, 590, width=530, height=32),
-                          Region(647, 338, width=530, height=32)]
-
 # My Messages Regions:
 MM_TITLE_REGION = Region(90, 60, width=180, height=40)
 MM_SUBTITLE_REGION = Region(310, 185, width=670, height=45)
--- tests/interactive_frame_objects.py
+++ tests/interactive_frame_objects.py
@@ -5,12 +5,29 @@ Test cases for Interactive menu
 """
 
 import stbt
-from stbt import FrameObject
+from stbt import FrameObject, Region
 from sky_plus_utils import debug, find_text, match_color
 import interactive_constants
 import sky_plus_strings
 
+
+# ##################### #
+# ##### Constants ##### #
+# ##################### #
+
+MAIN_MENU_ITEM_REGIONS = [Region(100, 338, width=530, height=32),
+                          Region(100, 374, width=530, height=32),
+                          Region(100, 410, width=530, height=32),
+                          Region(100, 446, width=530, height=32),
+                          Region(100, 482, width=530, height=32),
+                          Region(100, 518, width=530, height=32),
+                          Region(100, 554, width=530, height=32),
+                          Region(100, 590, width=530, height=32),
+                          Region(647, 338, width=530, height=32)]
+
+
 # pylint: disable=too-few-public-methods
+# TODO: Refactor?
 class MenuItem(object):
     """Class to store the attributes of a MySky menu item"""
 
@@ -24,8 +41,15 @@ class MenuItem(object):
         self.text = find_text(image, region)
         self.selected = match_color(image, region, interactive_constants.YELLOW_BACKGROUND_RGB)
 
+
 class InteractiveMainMenu(FrameObject):
     """FrameObject class to analyze Interactive main menu."""
+
+    def __init__(self, frame=None):
+        if frame is None:
+            frame = stbt.get_frame()
+        super(InteractiveMainMenu, self).__init__(frame)
+        self.items = []
 
     @property
     def is_visible(self):
@@ -45,15 +69,18 @@ class InteractiveMainMenu(FrameObject):
         selected_list = [x for x in self.menu_items if x.selected]
         return selected_list[0].text
 
+    def populate_items(self):
+        """Load menu items list"""
+        for region in MAIN_MENU_ITEM_REGIONS:
+            item = MenuItem(self._frame, region)
+            self.items.append(item)
+
     @property
     def menu_items(self):
         """Get menu items list"""
-        items = []
-        for region in interactive_constants.MAIN_MENU_ITEM_REGIONS:
-            item = MenuItem(self._frame, region)
-            items.append(item)
-
-        return items
+        if not self.items:
+            self.populate_items()
+        return self.items
 
 class MyMessagesMenu(FrameObject):
     """FrameObject class to analyze My Messages menu."""
